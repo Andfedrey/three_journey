7ea5e20efac0f5c9b43710a16221753422134493
Andrey <andreyfedorov2408@gmail.com>
2023-09-03 07:06:18 +0300

    object transform

---
+++ lesson_04/src/script.js
@@ -0,0 +1,27 @@
+import * as THREE from 'three'
+
+const scene = new THREE.Scene()
+
+const geometry = new THREE.BoxGeometry(1,1,1)
+const material = new THREE.MeshBasicMaterial({color: 'red'})
+
+const mesh = new THREE.Mesh(geometry, material)
+scene.add(mesh)
+
+const size = {
+  width: 800,
+  height: 600
+}
+
+const camera = new THREE.PerspectiveCamera(75, size.width/size.height)
+camera.position.z = 3;
+camera.position.x = 1;
+scene.add(camera)
+
+const canvas = document.getElementById('webgl')
+const renderer = new THREE.WebGLRenderer({
+  canvas
+})
+
+renderer.setSize(size.width, size.height)
+renderer.render(scene, camera)
